3f268ea0ccec18f22d2daaa1295bebded414fba6
k-shir0 <kakishiro2190394@gmail.com>
2023-03-17 00:39:37 +0900

    update: Header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
-
+import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind({
     config: { applyBaseStyles: false },
   })]
 });
