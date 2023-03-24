f4eb4f98ad7999f43ecd5837811cbd586c49be74
k-shir0 <kakishiro2190394@gmail.com>
2023-03-24 18:18:31 +0900

    fix: domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.ictsc.net/',
+  site: 'https://blog.icttoracon.net/',
   integrations: [mdx(), sitemap(), tailwind({
     config: { applyBaseStyles: false },
   })]
